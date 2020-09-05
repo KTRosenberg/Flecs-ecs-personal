d163300e3fff42b9764e7ac23a40f4e2f85ee1f6
KTRosenberg <ktr254@nyu.edu>
2020-09-05 15:44:55 -0400

    updated ALL

--- source/flecs_cpp_tools.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  flecs_cpp_tools.cpp
-//  Make The Thing
-//
-//  Created by Toby Rosenberg on 9/5/20.
-//  Copyright © 2020 Toby Rosenberg. All rights reserved.
-//
-
-#include "flecs_cpp_tools.h"
